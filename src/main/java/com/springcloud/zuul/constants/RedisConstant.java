4bd8c8001ded3ce968acf846e668203fa0bbad4b
zhangqi <1756834641@qq.com>
2019-12-26 15:38:25 +0800

    网关服务-鉴权

---
+++ src/main/java/com/springcloud/zuul/constants/RedisConstant.java
@@ -0,0 +1,11 @@
+package com.springcloud.zuul.constants;
+
+/**
+ * @Author: ZQ
+ * @Description:
+ * @Date created in 17:04 2019/12/23
+ */
+public class RedisConstant {
+
+    public static final String TOKEN_TEMPLATE = "token_%s";
+}
--- src/main/java/com/springcloud/zuul/filter/AuthFilter.java
+++ src/main/java/com/springcloud/zuul/filter/AuthFilter.java
@@ -3,9 +3,15 @@ package com.springcloud.zuul.filter;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
+import com.springcloud.zuul.Utils.CookieUtil;
+import com.springcloud.zuul.constants.RedisConstant;
 import org.apache.commons.lang.StringUtils;
+import org.apache.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_DECORATION_FILTER_ORDER;
@@ -18,6 +24,9 @@ import static org.springframework.cloud.netflix.zuul.filters.support.FilterConst
  */
 @Component
 public class AuthFilter extends ZuulFilter {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @Override
     public String filterType() {
         return PRE_TYPE;
@@ -42,6 +51,24 @@ public class AuthFilter extends ZuulFilter {
          * /order/finish 只能卖家访问
          * /product/list 都能访问
          */
+        if("/myOrder/order/create".equals(request.getRequestURI())){
+            Cookie cookie = CookieUtil.get(request,"openid");
+            if(cookie == null || StringUtils.isEmpty(cookie.getValue())){
+                requestContext.setSendZuulResponse(false);
+                requestContext.setResponseStatusCode(HttpStatus.SC_UNAUTHORIZED);
+            }
+        }
+
+        if("/myOrder/order/finish".equals(request.getRequestURI())){
+            Cookie cookie = CookieUtil.get(request,"token");
+            if(cookie == null
+                    || StringUtils.isEmpty(cookie.getValue() )
+                    || StringUtils.isEmpty(stringRedisTemplate.opsForValue().get(String.format(RedisConstant.TOKEN_TEMPLATE,cookie.getValue())))){
+                requestContext.setSendZuulResponse(false);
+                requestContext.setResponseStatusCode(HttpStatus.SC_UNAUTHORIZED);
+            }
+        }
+
         return null;
     }
 }
